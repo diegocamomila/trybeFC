fd8b3d5639a92b2fd56745aa66a6f6334df10e67
Diego Siqueira <diego.camomila@gmail.com>
2022-08-29 01:18:54 -0300

    Feat: endpoint /login/validate

--- app/backend/src/routes/userLogin.route.ts
+++ app/backend/src/routes/userLogin.route.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import UserLoginController from '../controllers/userLogin.controller';
+import AuthToken from '../Services/authorizationToken.service';
 
 const router = express.Router();
 
 router.post('/', (request, response) => UserLoginController.execute(request, response));
 
-// router.get('/validate', authMiddleware);
+router.get('/validate', (request, response) => AuthToken(request, response));
 
 export default router;
