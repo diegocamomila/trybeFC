ea1059543722651787c8c40b0fa2626cbfb8c0a2
Diego Siqueira <diego.camomila@gmail.com>
2022-08-31 22:30:11 -0300

    chore: requisitos 3, 5, 7, e 9

--- app/backend/src/controllers/LoginUser.controller.ts
+++ app/backend/src/controllers/LoginUser.controller.ts
@@ -1,6 +1,6 @@
 // import ILoginDTO from '../services/LoginService/loginDTO.service';
 
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import LoginUserServer from '../services/LoginService/LoginUser.service';
 
 export default class LoginUserController {
@@ -8,10 +8,14 @@ export default class LoginUserController {
     private loginUserServer: LoginUserServer,
   ) {}
 
-  async handlerLogin(request: Request, response: Response): Promise<Response | null> {
-    const { email, password } = request.body;
-    const token = await this.loginUserServer.execute(email, password);
-    return response.status(200).json({ token });
+  async handlerLogin(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { email, password } = request.body;
+      const token = await this.loginUserServer.execute(email, password);
+      return response.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
   }
 
   // static async validateToken(authorization?: string) {
--- app/backend/src/routes/LoginUser.route.ts
+++ app/backend/src/routes/LoginUser.route.ts
@@ -2,11 +2,11 @@ import * as express from 'express';
 import { loginUserController } from '../services/LoginService';
 
 const routes = express.Router();
-routes.post('/', loginUserController.handlerLogin);
+// routes.post('/', loginUserController.handlerLogin);
 
-// routes.post('/', (request, response) => {
-//   loginUserController.handlerLogin(request, response);
-// });
+routes.post('/', (request, response, next) => {
+  loginUserController.handlerLogin(request, response, next);
+});
 
 // router.get('/validate', (request, _response) => UserLoginController
 //   .validateToken(request.headers.authorization));
